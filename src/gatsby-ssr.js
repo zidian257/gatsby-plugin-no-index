8b8a6b7c06b3ab22ef649447572093ff2efaa247
zidian257@15in-mbp <zidian257@gmail.com>
2020-08-02 21:45:47 +0800

    first commit

---
+++ src/gatsby-ssr.js
@@ -0,0 +1,15 @@
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
+
+// You can delete this file if you're not using it
+import * as React from "react"
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  const NoFollow = (
+    <meta name="robots" content="noindex, nofollow, noimageindex" />
+  )
+  setHeadComponents([NoFollow])
+}
